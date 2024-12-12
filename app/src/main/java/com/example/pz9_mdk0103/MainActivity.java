04dde939a2554e5140aa1e66e5070162fb203ee1
marsox0d@outlook.com <marsox0d@outlook.com>
2024-12-12 11:21:10 +0500

    done

--- app/src/main/java/com/example/pz9_mdk0103/MainActivity.java
+++ app/src/main/java/com/example/pz9_mdk0103/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.pz9_mdk0103;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ListView listView = findViewById(R.id.lv);
+
+        String[] users = getResources().getStringArray(R.array.user_data);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.list_view, R.id.text, users);
+
+        listView.setAdapter(adapter);
     }
 }
